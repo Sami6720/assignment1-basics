d9d74786eb15f4765abe062a935be2fa27cafd86
sami6720 <saminur67@gmail.com>
2025-09-25 02:10:39 -0400

    Update trian.py so that if main block is in the bottom

--- transformer/train.py
+++ transformer/train.py
@@ -8,6 +8,68 @@ from transformer.modules import (Linear, Embedding, RMSNorm, Swiglu, RoPE, softm
 from einops import rearrange, reduce, repeat
 from einx import get_at
 
+def train(config):
+
+        d_model = config["d_model"]
+        vocab_size = config["vocab_size"]
+        context_length = config["context_length"]
+        d_model = config["d_model"]
+        d_ff = config["d_ff"]
+        rope_theta = config["rope_theta"]
+        num_layers = config["num_layers"]
+        num_heads = config["num_heads"]
+        total_tokens = config["total_tokens"]
+        checkpoint_path = config["checkpoint_path"]
+        weight_decay = config["weight_decay"]
+        batch_size = config["batch_size"]
+        device = config["device"]
+
+        import wandb
+        # wandb.init(
+        #     name=config["job_name"],
+        #     config=config,
+        #     entity=config["wandb_entity"],
+        #     project=config["wandb_project"],
+        #     mode=config['wandb_mode'],
+        #     save_code=True
+        # )
+
+        model = TransformerLM(d_model, num_heads, d_ff,
+                              context_length, rope_theta, vocab_size, num_layers)
+        optim = AdamW(model.parameters(), config["lr"], weight_decay)
+
+        # TODO: Need to figure out how to load the dataset.
+        dataset = np.load("tinystories_train.npy", mmap_mode='r')
+        validation_dataset = np.load("tinystories_val.npy", mmap_mode='r')
+
+        if checkpoint_path:
+            model = load_checkpoint(model, optim, checkpoint_path)
+
+        for i in range(config["num_epochs"]):
+            for j in range(config["training_steps_per_epoch"]):
+                optim.zero_grad()
+                X, Y = get_batch(dataset, context_length, batch_size, device)
+                X = model(X)  # B, T, V
+                X = rearrange(X, "b t v -> t b v")
+                Y = rearrange(Y, "b t -> t b")
+                loss = cross_entropy(X, Y)
+                loss.backward()
+                optim.step()
+
+                metric = {
+                    "training_loss": loss.mean().item(),
+                    "epoch": i,
+                    "training_step_in_epoch": j
+                }
+
+                validate_interval = j % config["validate_every_x_steps"]
+                if validate_interval:
+                    X, Y = get_batch(validation_dataset, context_length, batch_size, device)
+                    X = model(X)  # B, T, V
+                    X = rearrange(X, "b t v -> t b v")
+                    Y = rearrange(Y, "b t -> t b")
+                    loss = cross_entropy(model(X), Y)
+                    metric["validation_loss"] = loss.mean().item()
 
 if __name__ == '__main__':
 
@@ -32,83 +94,31 @@ if __name__ == '__main__':
     parser.add_argument("--num_layers", type=int, default=4,
                         help="Number of transformer layers.")
 
+    parser.add_argument("--num_epochs", type=int, default=4,
+                        help="Num of training epochs")
+
+    parser.add_argument("--training_steps_per_epoch", type=int, default=4,
+                        help="Training steps per epoch.")
+
     parser.add_argument("--num_heads", type=int, default=16,
                         help="Number of attention heads.")
 
-    parser.add_argument("--batch_size", type=int, default=16,
+    parser.add_argument("--batch_size", type=int, default=4,
                         help="Minibatch size")
 
     parser.add_argument("--total_tokens", type=int, default=327_680_000,
                         help="Total tokens processed (batch_size × steps × context_length).")
+    parser.add_argument("--validate_every_x_steps", type=int, default=327_680_000,
+                        help="How often to validate.")
 
     parser.add_argument("--lr", type=float, default=1e-4,
                         help="Learning rate")
     parser.add_argument("--checkpoint_path", type=str, default=None)
+    parser.add_argument("--job_name", type=str, default="debug")
     parser.add_argument("--weight_decay", type=float, default=0.9)
     parser.add_argument("--device", type=str, default="cpu")
 
-    args = parser.parse_args()
+    args = vars(parser.parse_args())
 
+    train(args)
 
-    def train(config):
-
-        d_model = config["d_model"]
-        vocab_size = config["vocab_size"]
-        context_length = config["context_length"]
-        d_model = config["d_model"]
-        d_ff = config["d_ff"]
-        rope_theta = config["rope_theta"]
-        num_layers = config["num_layers"]
-        num_heads = config["num_heads"]
-        total_tokens = config["total_tokens"]
-        checkpoint_path = config["checkpoint_path"]
-        weight_decay = config["weight_decay"]
-        batch_size = config["batch_size"]
-        device = config["device"]
-
-        import wandb
-        wandb.init(
-            name=config["job_name"],
-            config=config,
-            entity=config["wandb_entity"],
-            project=config["wandb_project"],
-            mode=config['wandb_mode'],
-            save_code=True
-        )
-
-        model = TransformerLM(d_model, num_heads, d_ff,
-                              context_length, rope_theta, vocab_size, num_layers)
-        optim = AdamW(model.parameters(), weight_decay)
-
-        # TODO: Need to figure out how to load the dataset.
-        dataset = np.load("tinystories_train.npy", mmap_mode='r')
-        validation_dataset = np.load("tinystories_val.npy", mmap_mode='r')
-
-        if checkpoint_path:
-            model = load_checkpoint(model, optim, checkpoint_path)
-
-        for i in range(config["num_epochs"]):
-            for j in range(config["training_steps_per_epoch"]):
-                optim.zero_grad()
-                X, Y = get_batch(dataset, context_length, batch_size, device)
-                X = model(X)  # B, T, V
-                X = rearrange(X, "b t v -> t b v")
-                Y = rearrange(Y, "b t -> t b")
-                loss = cross_entropy(model(X), Y)
-                loss.backward()
-                optim.step()
-
-                metric = {
-                    "training_loss": loss.mean().item(),
-                    "epoch": i,
-                    "training_step_in_epoch": j
-                }
-
-                validate_interval = j % config["validate_every_x_steps"]
-                if validate_interval:
-                    X, Y = get_batch(validation_dataset, context_length, batch_size, device)
-                    X = model(X)  # B, T, V
-                    X = rearrange(X, "b t v -> t b v")
-                    Y = rearrange(Y, "b t -> t b")
-                    loss = cross_entropy(model(X), Y)
-                    metric["validation_loss"] = loss.mean().item()
