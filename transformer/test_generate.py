f70afe72cc8bd6fe7350d8c36ee29d3190ad497c
sami6720 <saminur67@gmail.com>
2025-09-27 16:08:42 -0400

    Updte test gen

--- transformer/test_generate.py
+++ transformer/test_generate.py
@@ -87,5 +87,4 @@ if __name__ == '__main__':
                           context_length, rope_theta, vocab_size, num_layers, tokenizer=tokenizer)
     model.eval()
 
-    print(model.generate("<bos>", max_generation_len=20))
-
+    print(model.generate("Once upon a time", max_generation_len=20))
